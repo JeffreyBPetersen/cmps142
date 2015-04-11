5233f51f1a8a06fee47184a7e4694ce49209f7b0
Jeffrey Petersen <jeffreybpetersen@gmail.com>
2015-04-11 12:49:31 -0700

    generate training data

--- hw1/question5.py
+++ hw1/question5.py
@@ -1,2 +1,13 @@
-print "Hello World\n"
+import random
+
+print "UCSC Machine Learning hw1q5\n"
+
+training = []
+for i in range(500):
+  training.append([])
+  for j in range(11):
+    training[i].append(random.choice([-1,1]))
+
+print training
+
 raw_input("--- press any key to end program ---")
