c35612a0fe0cee46d3974a98d7b557f188ecfbec
Jeffrey Petersen <jeffreybpetersen@gmail.com>
2015-04-13 18:35:53 -0700

    refactor, implement labeling modes

--- hw1/question5.py
+++ hw1/question5.py
@@ -1,13 +1,46 @@
 import random
 
+# named constants
+training_size = 500
+testing_size = 500
+dimensionality = 11
+learning_rate = 1 # set within range [0,1]
+
+## implement label_instance(mode) # mode is 'a', 'b', or 'c'
+## implement update_weights(weights)
+
+def generate_instances(quantity, dimensionality):
+  instances = []
+  for instance in range(quantity):
+    instances.append([])
+    for component in range(dimensionality):
+      instances[instance].append(random.choice([-1,1]))
+  return instances
+
+def label_instances(instances, labelling_mode):
+  labelled_instances = []
+  for instance in instances:
+    if labelling_mode == "a":
+      labelled_instances.append((instance, 1 if instance[0] == 1 else 0))
+    elif labelling_mode == "b":
+      labelled_instances.append((instance, 1 if sum(instance) > 0 else 0))
+    elif labelling_mode == "c":
+      labelled_instances.append((instance, 1 if sum(instance) + random.randrange(-4,4) >= 0 else 0))
+    else:
+      print "invalid labelling mode"
+  return labelled_instances
+
 print "UCSC Machine Learning hw1q5\n"
 
-training = []
-for i in range(500):
-  training.append([])
-  for j in range(11):
-    training[i].append(random.choice([-1,1]))
+# generate instances as [components]
+training = generate_instances(training_size, dimensionality)
+testing = generate_instances(testing_size, dimensionality)
+# label training instances as ([components], label)
+labelling_mode = raw_input("Select experiment - enter a, b, or c: ")
+training = label_instances(training, labelling_mode)
+testing = label_instances(testing, labelling_mode)
 
-print training
+for instance in training:
+  print instance
 
 raw_input("--- press any key to end program ---")
