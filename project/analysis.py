fb753cfcae3862ea8468bd62b1b54dffb106bc04
Raymond Colebaugh <raymondcolebaugh@gmail.com>
2015-05-26 11:44:00 -0700

    Added more methods to analysis script for generating classifiers

--- project/analysis.py
+++ project/analysis.py
@@ -4,7 +4,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn import tree
+from sklearn import svm, tree, ensemble
 
 def build_nb_classifier(df):
     """ Construct a classifier Naive Bayes
@@ -18,7 +18,12 @@ def build_svm(df):
 
     :param df: Pandas Data Frame
     """
-    pass
+    # Separate observations and target
+    features = ['Age', 'Gender', 'Pclass']
+    X = df[features]
+    y = df['Survived']
+    svc = svm.LinearSVC()
+    #svc.fit(X, y)
 
 def build_decision_tree(df):
     """ Construct a classifier using a decision tree
@@ -26,18 +31,41 @@ def build_decision_tree(df):
     :param df: Pandas Data Frame
     """
     # Separate observations and target
-    X = df.copy()
-    del X['Survived']
+    features = ['Age', 'Gender', 'Pclass']
+    X = df[features]
     y = df['Survived']
     decision_tree = tree.DecisionTreeClassifier()
-    # TODO finish converting data into expected format
     #decision_tree = decision_tree.fit(X, y)
+
+def build_random_forest(df):
+    """ Construct a classifier using a random forest
+
+    :param df: Pandas Data Frame
+    """
+    # Separate observations and target
+    features = ['Age', 'Gender', 'Pclass']
+    X = df[features]
+    y = df['Survived']
+    forest = ensemble.RandomForestClassifier()
+    #forest = forest.fit(X, y)
+
+def munge_data(df):
+    """ Work the data into an appropriate form for analysis
+
+    :param df: Pandas Data Frame
+    """
+    # Create a new Gender column from Sex with the mapping female -> 0, male -> 1
+    df['Gender'] = df['Sex'].map(lambda x: 1 if x == 'male' else 0)
+    # TODO finish converting data into expected format
+    return df
 
 def main():
     # Load Titanic passenger csv into a data frame
     df = pd.read_csv('train.csv', header=0)
+    df = munge_data(df)
     survivors = df[(df['Survived'] == 1)]
     perishers = df[(df['Survived'] == 0)]
+
     print("Survival Rate: %.2f" % df["Survived"].mean())
     print("Average Age: %.2f" % df["Age"].mean())
     print("Average Age of Survivor: %.2f" % survivors["Age"].mean())
